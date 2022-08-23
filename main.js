7ddee9bb179cc2a9926f7ec654acea043a133085
Bernardo <82006675+BernardoQ@users.noreply.github.com>
2022-08-23 12:10:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,31 +14,30 @@ const multiplicar = (num1,num2)=>{
     return parseInt(num1) * parseInt(num2);
 }
 
-alert("Que operacion desea realizar?");
-let operacion = prompt("1: suma, 2: resta, 3: division, 4: multiplicacion");
+let operacion = prompt("¿Que operacion desea realizar? \n 1: suma, 2: resta, 3: division, 4: multiplicacion");
 
-if(operacion == 1) {
+if(operacion == 1 || operacion == "suma") {
     let numero1 = prompt("ingrese el primer numero a operar");
     let numero2 = prompt("ingrese el segundo numero a operar");
     resultado = sumar(numero1, numero2);
     alert(`El resultado es ${resultado}`);
 }
 
-else if(operacion == 2) {
+else if(operacion == 2 || operacion == "resta") {
     let numero1 = prompt("ingrese el primer numero a operar");
     let numero2 = prompt("ingrese el segundo numero a operar");
     resultado = restar(numero1, numero2);
     alert(`El resultado es ${resultado}`);
 }
 
-else if(operacion == 3) {
+else if(operacion == 3 || operacion == "division") {
     let numero1 = prompt("ingrese el primer numero a operar");
     let numero2 = prompt("ingrese el segundo numero a operar");
     resultado = dividir(numero1, numero2);
     alert(`El resultado es ${resultado}`);
 }
 
-else if(operacion == 4) {
+else if(operacion == 4 || operacion == "multiplicacion") {
     let numero1 = prompt("ingrese el primer numero a operar");
     let numero2 = prompt("ingrese el segundo numero a operar");
     resultado = multiplicar(numero1, numero2);
@@ -48,5 +47,6 @@ else if(operacion == 4) {
 else{
     alert("Seleccione un parametro correcto");
 }
+
 
 
