4263dfdbb1ee7669e8ce83c2cc31bbe186caacd0
Bernardo <82006675+BernardoQ@users.noreply.github.com>
2022-08-23 21:51:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,59 @@
 //bucles for - while - do...while
+
+//if - else if - else
 while(true){
+    const sumar = (num1,num2)=>{
+        return parseInt(num1) + parseInt(num2);5
+    }
+
+    const restar = (num1,num2)=>{
+        return parseInt(num1) - parseInt(num2);
+    }
+
+    const dividir = (num1,num2)=>{
+        return parseInt(num1) / parseInt(num2);
+    }
+
+    const multiplicar = (num1,num2)=>{
+        return parseInt(num1) * parseInt(num2);
+    }
+
+    let operacion = prompt("¿Que operacion desea realizar? \n 1: suma, 2: resta, 3: division, 4: multiplicacion");
+
+    if(operacion == 1 || operacion == "suma") {
+        let numero1 = prompt("ingrese el primer numero a operar");
+        let numero2 = prompt("ingrese el segundo numero a operar");
+        resultado = sumar(numero1, numero2);
+        alert(`El resultado es ${resultado}`);
+    }
+
+    else if(operacion == 2 || operacion == "resta") {
+        let numero1 = prompt("ingrese el primer numero a operar");
+        let numero2 = prompt("ingrese el segundo numero a operar");
+        resultado = restar(numero1, numero2);
+        alert(`El resultado es ${resultado}`);
+    }
+
+    else if(operacion == 3 || operacion == "division") {
+        let numero1 = prompt("ingrese el primer numero a operar");
+        let numero2 = prompt("ingrese el segundo numero a operar");
+        resultado = dividir(numero1, numero2);
+        alert(`El resultado es ${resultado}`);
+    }
+
+    else if(operacion == 4 || operacion == "multiplicacion") {
+        let numero1 = prompt("ingrese el primer numero a operar");
+        let numero2 = prompt("ingrese el segundo numero a operar");
+        resultado = multiplicar(numero1, numero2);
+        alert(`El resultado es ${resultado}`);
+    }
+
+    else{
+        alert("Seleccione un parametro correcto");
+    } 
+}
+
+/*while(true){
     let venta = prompt ('Introduzca el monto de venta entre 0 y 100:');
     if(venta > 100 || venta < 0 || isNaN(venta)){
                 alert ('¡Error de entrada, vuelva a ingresar!');
@@ -18,59 +72,6 @@ while(true){
         break;
         }
     }
-}
-
-
-//if - else if - else
-const sumar = (num1,num2)=>{
-    return parseInt(num1) + parseInt(num2);
-}
-
-const restar = (num1,num2)=>{
-    return parseInt(num1) - parseInt(num2);
-}
-
-const dividir = (num1,num2)=>{
-    return parseInt(num1) / parseInt(num2);
-}
-
-const multiplicar = (num1,num2)=>{
-    return parseInt(num1) * parseInt(num2);
-}
-
-let operacion = prompt("¿Que operacion desea realizar? \n 1: suma, 2: resta, 3: division, 4: multiplicacion");
-
-if(operacion == 1 || operacion == "suma") {
-    let numero1 = prompt("ingrese el primer numero a operar");
-    let numero2 = prompt("ingrese el segundo numero a operar");
-    resultado = sumar(numero1, numero2);
-    alert(`El resultado es ${resultado}`);
-}
-
-else if(operacion == 2 || operacion == "resta") {
-    let numero1 = prompt("ingrese el primer numero a operar");
-    let numero2 = prompt("ingrese el segundo numero a operar");
-    resultado = restar(numero1, numero2);
-    alert(`El resultado es ${resultado}`);
-}
-
-else if(operacion == 3 || operacion == "division") {
-    let numero1 = prompt("ingrese el primer numero a operar");
-    let numero2 = prompt("ingrese el segundo numero a operar");
-    resultado = dividir(numero1, numero2);
-    alert(`El resultado es ${resultado}`);
-}
-
-else if(operacion == 4 || operacion == "multiplicacion") {
-    let numero1 = prompt("ingrese el primer numero a operar");
-    let numero2 = prompt("ingrese el segundo numero a operar");
-    resultado = multiplicar(numero1, numero2);
-    alert(`El resultado es ${resultado}`);
-}
-
-else{
-    alert("Seleccione un parametro correcto");
-} 
-
+}*/
 
 
